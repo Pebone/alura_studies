f12e0019e97fd5c9378e321bc8fbdcb2a64d49e5
Davi Pereira <58438668+Pebone@users.noreply.github.com>
2024-02-14 18:51:55 -0300

    Add files via upload

---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,27 @@
+import { ITarefa } from "../../types/tarefa";
+import style from "./Lista.module.scss";
+import Item from "./item";
+
+interface Props {
+  tarefas: ITarefa[];
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void;
+}
+
+function Lista({ tarefas, selecionaTarefa }: Props) {
+  return (
+    <aside className={style.listaTarefas}>
+      <h2>Estudos do dia</h2>
+      <ul>
+        {tarefas.map((item) => (
+          <Item
+            selecionaTarefa={selecionaTarefa}
+            key={item.id}
+            {...item}
+          ></Item>
+        ))}
+      </ul>
+    </aside>
+  );
+}
+
+export default Lista;
